54d00d9331b0f49156c83746313fcea06e3d07c9
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2024-03-17 19:58:47 +0800

    pokelist done

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,10 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
-
   // Collection Reference
-  final CollectionReference pokemonCollection = FirebaseFirestore.instance.collection('pokemons');
+  final CollectionReference pokemonCollection =
+  FirebaseFirestore.instance.collection('pokemons');
 
-  Future updateUserData()
-
+  Future<void> updateUserData(String userId, Map<String, dynamic> userData) async {
+    try {
+      await pokemonCollection.doc(userId).update(userData);
+      print('User data updated successfully');
+    } catch (error) {
+      print('Failed to update user data: $error');
+      throw error; // Rethrow the error to handle it in the calling code if necessary
+    }
+  }
 }
